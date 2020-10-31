6f4a182b7f43976e2337e3f253c39195b85d980a
TeoSparkles <catapusantp@byui.edu>
2020-10-31 10:55:19 +0800

    Updated from TeoSparkles

--- app/src/main/java/com/example/teamprojectcs246/MainActivity.java
+++ app/src/main/java/com/example/teamprojectcs246/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         System.out.println("Hello Billy, Teo here.");
+        System.out.println("Updated");
     }
 }
