9da446f19d7e3b9a2d57f0c4a912c93f4df9b10a
suchjose84 <65042405+suchjose84@users.noreply.github.com>
2020-10-31 15:07:47 +0800

    added my update

--- app/src/main/java/com/example/teamprojectcs246/MainActivity.java
+++ app/src/main/java/com/example/teamprojectcs246/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("Update from Teo");
-        System.out.println("Update from Joe");
+        System.out.println("Update from Jojo");
     }
 }
